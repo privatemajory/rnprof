44253d8329d40fc2ada95ed0364121d262f7d33c
privatemajory <privatemajory@gmail.com>
2019-05-20 21:06:13 +0300

    First commit

---
+++ distances.js
@@ -0,0 +1,43 @@
+var distancesN2 = [ 
+
+      { dist: 0, place: 'Antananarivo' },
+      { dist: 38129, place: 'Manjakandriana' },
+      { dist: 93619, place: 'Moramanga'},
+      { dist: 210156, place: 'Brickaville'},
+      { dist: 303554, place: 'Toamasina'}
+
+     ];
+
+var distancesN7 = [ 
+
+      { dist: 0, place: 'Antananarivo' },
+      { dist: 146733, place: 'Antsirabe'},
+      { dist: 229564, place: 'Ambositra'},
+      { dist: 353674, place: 'Fianarantsoa'},
+      { dist: 534169, place: 'Ihosy'},
+      { dist: 725186, place: 'Sakaraha'},
+      { dist: 846148, place: 'Toliara'}
+
+     ];
+
+var distancesN4 = [
+
+      { dist: 0, place: 'Antananarivo' },
+      { dist: 84684, place: 'Ankazobe' },
+      { dist: 283594, place: 'Maevatanàna' },
+      { dist: 370253, place: 'Ambondromamy' },
+      { dist: 519956, place: 'Mahajanga' }
+
+];
+
+var distancesN6 = [
+
+      { dist: 0, place: 'Ambondromamy' },
+      { dist: 78511, place: 'Mampikony' },
+      { dist: 151359, place: 'Port-Bergé' },
+      { dist: 268528, place: 'Antsohihy' },
+      { dist: 446146, place: 'Ambanja' },
+      { dist: 544344, place: 'Ambilobe' },
+      { dist: 670791, place: 'Antsiranana'}
+
+]
